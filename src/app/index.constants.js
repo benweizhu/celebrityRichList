48a821fbeb0eab5875811ca3f768baaf4ef61632
Benwei <benweizhu@gmail.com>
2015-11-04 09:30:35 +0800

    [celebrityRichList] [Benwei] initialize project with yo gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('celebrityRichList')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
